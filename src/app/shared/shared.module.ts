2030ad977f8003f658a010d76da5c0d0969a945d
unknown <zerontecnologia1@gmail.com>
2019-05-23 23:24:57 -0400

    creacion de modulos y modificacion de rutas

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+
+
+@NgModule({
+
+    declarations:[
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent,
+],
+exports:[
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent,
+]
+
+})
+
+export class SharedModule{}
